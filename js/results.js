34a8499c7f3f70e0c5eb9ce2e9673a663ffb7971
Ayodele Olalekan <longyarnz@gmail.com>
2019-12-02 06:20:44 +0100

    Retrieve stored data from cache

--- js/results.js
+++ js/results.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-
+  populateFields();
 })
+
+function populateFields() {
+  let data = localStorage.getItem('amadeus_data');
+  if (!data) return;
+
+ data = JSON.parse(data);
+ console.info(data);
+}
